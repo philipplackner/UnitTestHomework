d9599ce730973789114bccd3c56bc98717e0dbaf
Philipp <mike.litoris@gmail.com>
2020-08-11 19:17:26 +0200

    Initial commit

---
+++ app/src/main/java/com/androiddevs/unittesthomework/Homework.kt
@@ -0,0 +1,34 @@
+package com.androiddevs.unittesthomework
+
+object Homework {
+
+    /**
+     * Returns the n-th fibonacci number
+     * They are defined like this:
+     * fib(0) = 0
+     * fib(1) = 1
+     * fib(n) = fib(n-2) + fib(n-1)
+     */
+    fun fib(n: Int): Long {
+        if(n == 0 || n == 1) {
+            return n.toLong()
+        }
+        var a = 0L
+        var b = 1L
+        var c = 1L
+        (1..n-2).forEach { i ->
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+
+    /**
+     * Checks if the braces are set correctly
+     * e.g. "(a * b))" should return false
+     */
+    fun checkBraces(string: String): Boolean {
+        return string.count { it == '(' } == string.count { it == ')' }
+    }
+}
